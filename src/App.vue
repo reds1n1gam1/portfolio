a2825971890285ad832094cc2d8a1e65eb921777
Dima <reds1n1gam1@gmail.com>
2026-05-03 22:58:57 +0500

    - Removed SCSS - Added shadcn

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import Button from './components/ui/button/Button.vue';
+
 
 </script>
 
 <template>
-  App
+  <Button variant="outline">Button test</Button>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './styles/style.scss'
+import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
